a7f275448fce9dabefcb9fe7290ab3fa86febd92
Victor Porof <victor.porof@gmail.com>
2018-02-19 11:01:49 +0100

    Stop using transmute for callbacks
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ pub use types::*;
 #[repr(C)]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Node {
-	inner_node: NodeRef,
+	inner_node: YGInternalNodeRef,
 	should_free: bool,
 }
 
@@ -495,7 +495,7 @@ impl Node {
 		unsafe { internal::YGNodeGetChildCount(self.inner_node) }
 	}
 
-	pub fn get_child(&self, index: u32) -> NodeRef {
+	pub fn get_child(&self, index: u32) -> YGInternalNodeRef {
 		unsafe { internal::YGNodeGetChild(self.inner_node, index) }
 	}
 
@@ -872,35 +872,15 @@ impl Node {
 		}
 	}
 
-	pub fn set_measure_func(&mut self, func: MeasureFunc) {
-		match func {
-			Some(f) => unsafe {
-				type Callback = unsafe extern "C" fn(
-					internal::YGNodeRef,
-					f32,
-					internal::YGMeasureMode,
-					f32,
-					internal::YGMeasureMode,
-				) -> internal::YGSize;
-				let casted_func: Callback = std::mem::transmute(f as usize);
-				internal::YGNodeSetMeasureFunc(self.inner_node, Some(casted_func));
-			},
-			None => unsafe {
-				internal::YGNodeSetMeasureFunc(self.inner_node, None);
-			},
+	pub fn set_measure_func(&mut self, func: internal::YGMeasureFunc) {
+		unsafe {
+			internal::YGNodeSetMeasureFunc(self.inner_node, func);
 		}
 	}
 
-	pub fn set_baseline_func(&mut self, func: BaselineFunc) {
-		match func {
-			Some(f) => unsafe {
-				type Callback = unsafe extern "C" fn(internal::YGNodeRef, f32, f32) -> f32;
-				let casted_func: Callback = std::mem::transmute(f as usize);
-				internal::YGNodeSetBaselineFunc(self.inner_node, Some(casted_func));
-			},
-			None => unsafe {
-				internal::YGNodeSetBaselineFunc(self.inner_node, None);
-			},
+	pub fn set_baseline_func(&mut self, func: internal::YGBaselineFunc) {
+		unsafe {
+			internal::YGNodeSetBaselineFunc(self.inner_node, func);
 		}
 	}
 
@@ -911,12 +891,12 @@ impl Node {
 		unsafe { internal::YGNodeSetContext(self.inner_node, raw) }
 	}
 
-	pub fn get_context(node_ref: &NodeRef) -> Option<&Box<Any>> {
+	pub fn get_context(node_ref: &YGInternalNodeRef) -> Option<&Box<Any>> {
 		let raw = unsafe { internal::YGNodeGetContext(*node_ref) };
 		Context::get_inner_ref(raw)
 	}
 
-	pub fn get_context_mut(node_ref: &NodeRef) -> Option<&mut Box<Any>> {
+	pub fn get_context_mut(node_ref: &YGInternalNodeRef) -> Option<&mut Box<Any>> {
 		let raw = unsafe { internal::YGNodeGetContext(*node_ref) };
 		Context::get_inner_mut(raw)
 	}
